0453ede1e671bdabda7c035dbc292d2d1846d7b3
David <dheyved1@gmail.com>
2022-10-13 02:07:26 +0100

    home and footer component

--- src/app/default/components/default-footer/default-footer.component.ts
+++ src/app/default/components/default-footer/default-footer.component.ts
@@ -3,13 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-default-footer',
   templateUrl: './default-footer.component.html',
-  styleUrls: ['./default-footer.component.scss']
+  styleUrls: ['./default-footer.component.scss'],
 })
 export class DefaultFooterComponent implements OnInit {
+  currentYear: any;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    // Update the element to the current year //
+    this.currentYear = new Date().getFullYear();
   }
-
 }
