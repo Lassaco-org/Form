e6f947027848acec81d594740a63be4aa0312e86
David <dheyved1@gmail.com>
2022-10-17 02:48:16 +0100

    display response

--- src/app/admin/components/admin-surveys/display-survey-responses/display-survey-responses.component.ts
+++ src/app/admin/components/admin-surveys/display-survey-responses/display-survey-responses.component.ts
@@ -1,4 +1,55 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  Chart,
+  ArcElement,
+  LineElement,
+  BarElement,
+  PointElement,
+  BarController,
+  BubbleController,
+  DoughnutController,
+  LineController,
+  PieController,
+  PolarAreaController,
+  RadarController,
+  ScatterController,
+  CategoryScale,
+  LinearScale,
+  LogarithmicScale,
+  RadialLinearScale,
+  TimeScale,
+  TimeSeriesScale,
+  Filler,
+  Legend,
+  Title,
+  Tooltip,
+} from 'chart.js';
+
+// Register the Chart Elements
+Chart.register(
+  ArcElement,
+  LineElement,
+  BarElement,
+  PointElement,
+  BarController,
+  BubbleController,
+  DoughnutController,
+  LineController,
+  PieController,
+  PolarAreaController,
+  RadarController,
+  ScatterController,
+  CategoryScale,
+  LinearScale,
+  LogarithmicScale,
+  RadialLinearScale,
+  TimeScale,
+  TimeSeriesScale,
+  Filler,
+  Legend,
+  Title,
+  Tooltip
+);
 
 @Component({
   selector: 'app-display-survey-responses',
@@ -6,7 +57,96 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./display-survey-responses.component.scss'],
 })
 export class DisplaySurveyResponsesComponent implements OnInit {
+  public chart: any;
+  isAlert: boolean = false;
+  alertMessage: string = '';
+  alertColor: string = '';
+  isShareModal: boolean = false;
+  formId: string = '';
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.createChart();
+  }
+
+  createChart() {
+    this.chart = new Chart('chart1', {
+      type: 'bar',
+
+      data: {
+        // values on X-Axis
+        labels: [
+          '2022-05-10',
+          '2022-05-11',
+          '2022-05-12',
+          '2022-05-13',
+          '2022-05-14',
+          '2022-05-15',
+          '2022-05-16',
+          '2022-05-17',
+        ],
+        datasets: [
+          {
+            label: 'Sales',
+            data: ['467', '576', '572', '79', '92', '574', '573', '576'],
+            backgroundColor: 'blue',
+          },
+          {
+            label: 'Profit',
+            data: ['542', '542', '536', '327', '17', '0.00', '538', '541'],
+            backgroundColor: 'limegreen',
+          },
+        ],
+      },
+      options: {
+        responsive: true,
+        aspectRatio: 2.5,
+      },
+    });
+
+    this.chart = new Chart('chart2', {
+      type: 'doughnut',
+
+      data: {
+        // values on X-Axis
+        labels: [
+          '2022-05-10',
+          '2022-05-11',
+          '2022-05-12',
+          '2022-05-13',
+          '2022-05-14',
+          '2022-05-15',
+          '2022-05-16',
+          '2022-05-17',
+        ],
+        datasets: [
+          {
+            label: 'Sales',
+            data: ['467', '576', '572', '79', '92', '574', '573', '576'],
+            backgroundColor: 'blue',
+          },
+          {
+            label: 'Profit',
+            data: ['542', '542', '536', '327', '17', '0.00', '538', '541'],
+            backgroundColor: 'limegreen',
+          },
+        ],
+      },
+      options: {
+        responsive: true,
+        aspectRatio: 2.5,
+      },
+    });
+  }
+
+  // Open share modal
+  openShareModal(formId: any) {
+    this.formId = formId;
+    this.isShareModal = true;
+  }
+
+  // close share modal
+  closeShareModal() {
+    this.isShareModal = false;
+  }
 }
