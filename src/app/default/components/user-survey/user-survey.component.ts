c4a3fe19eb5582b82a10ac426e2e700363541d9f
David <dheyved1@gmail.com>
2022-10-19 01:15:12 +0100

    background color to dash header

--- src/app/default/components/user-survey/user-survey.component.ts
+++ src/app/default/components/user-survey/user-survey.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-user-survey',
   templateUrl: './user-survey.component.html',
-  styleUrls: ['./user-survey.component.scss']
+  styleUrls: ['./user-survey.component.scss'],
 })
 export class UserSurveyComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
